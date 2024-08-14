94db58a4c932376e1595cb292d8cc9512e76704d
Odhi-ambo <michaelderrick605@gmail.com>
2024-08-14 09:01:19 +0300

    switching between the themes

--- lib/pages/themes/theme_provider.dart
+++ lib/pages/themes/theme_provider.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/pages/themes/dark_mode.dart';
+import 'package:habit_tracker/pages/themes/light_mode.dart';
 
 class ThemeProvider extends ChangeNotifier {
   //initially starts as darkmode
 
   ThemeData _themeData = darkMode;
 
-
   //get current theme
+  ThemeData get themeData => _themeData;
+
+  //is current theme lightmode
+  bool get isLightMode => _themeData == lightMode;
+
+  //set theme
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
 }
