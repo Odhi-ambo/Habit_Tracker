4040b8b83ffca98c719f50627ad7e89b1df3cb06
Odhi-ambo <michaelderrick605@gmail.com>
2024-08-14 08:55:25 +0300

    switching between the themes

---
+++ lib/pages/themes/theme_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/pages/themes/dark_mode.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  //initially starts as darkmode
+
+  ThemeData _themeData = darkMode;
+
+
+  //get current theme
+}
