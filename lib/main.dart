5728964779de4b9d293cb7173d6d8606b7e1e0da
Odhi-ambo <michaelderrick605@gmail.com>
2024-08-15 08:57:50 +0300

    state management with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/pages/my_home_page.dart';
+import 'package:habit_tracker/pages/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
