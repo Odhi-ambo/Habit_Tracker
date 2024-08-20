b98c96e7d43ed4190b621ce4c582323e653ac059
Odhi-ambo <michaelderrick605@gmail.com>
2024-08-20 09:00:14 +0300

    initial commit for app model

--- lib/models/habit.dart
+++ lib/models/habit.dart
@@ -3,4 +3,10 @@ import 'package:isar/isar.dart';
 class Habit {
   //habit id
   Id id = Isar.autoIncrement;
+
+  //habit name
+  late String name;
+
+  //completed days
+  List<DateTime> completedDays = [];
 }
