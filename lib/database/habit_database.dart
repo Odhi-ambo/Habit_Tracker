6bdc937ccf1b9849b0752c8ae1fb02cfd8d6908a
Odhi-ambo <michaelderrick605@gmail.com>
2024-08-21 07:44:22 +0300

    DB implemetantion

--- lib/database/habit_database.dart
+++ lib/database/habit_database.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/models/app_settings.dart';
+import 'package:habit_tracker/models/habit.dart';
+import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
 class HabitDatabase extends ChangeNotifier {}
@@ -11,4 +14,8 @@ setup
 //INITIALIZE DB
 Future<void> initialize() async {
   final dir = await getApplicationDocumentsDirectory();
+  isar = await Isar.open(
+    [HabitSchema, AppSettingsSchema],
+    directory: dir.path,
+  );
 }
