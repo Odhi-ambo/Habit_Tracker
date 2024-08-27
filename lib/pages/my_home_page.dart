89f896f753326810b343e9081db7d9d12c34a498
Odhi-ambo <michaelderrick605@gmail.com>
2024-08-27 07:33:09 +0300

    Ui design

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:habit_tracker/pages/themes/theme_provider.dart';
-import 'package:provider/provider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
