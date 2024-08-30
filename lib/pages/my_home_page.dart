c4e054be2809929d65b0cf5929cca3ece73ee616
Odhi-ambo <michaelderrick605@gmail.com>
2024-08-30 08:35:09 +0300

    Ui design

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -19,15 +19,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 controller: textController,
                 decoration:
                     const InputDecoration(hintText: 'Create a new habit'),
-                    
               ),
               actions: [
-
                 //save button
-                MaterialButton(onPressed: (){})
-
-
-
+                MaterialButton(onPressed: () {
+                  //get the new habit
+                  String newHabitName = textController.text;
+                })
               ],
             ));
   }
