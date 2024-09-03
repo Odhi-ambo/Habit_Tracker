11ca57945b5d098813715466dc995afc396b7e5d
Odhi-ambo <michaelderrick605@gmail.com>
2024-09-03 07:48:13 +0300

    my habit tile

--- lib/components/my_habit_tile.dart
+++ lib/components/my_habit_tile.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class MyHabitTile extends StatelessWidget {
   final bool isCompleted;
-  final Text;
-  const MyHabitTile({super.key, required this.isCompleted, required this.Text});
+  final text;
+  const MyHabitTile({super.key, required this.isCompleted, required this.text});
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +12,7 @@ class MyHabitTile extends StatelessWidget {
           color: isCompleted
               ? Colors.green
               : Theme.of(context).colorScheme.secondary),
-      child: Text(Text),
+      child: Text(text),
     );
   }
 }
