0357019019a07986b230055bc3029ad0473a397c
Odhi-ambo <michaelderrick605@gmail.com>
2024-09-05 08:36:02 +0300

    my habit tile

--- lib/components/my_habit_tile.dart
+++ lib/components/my_habit_tile.dart
@@ -12,21 +12,24 @@ class MyHabitTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-            color: isCompleted
-                ? Colors.green
-                : Theme.of(context).colorScheme.secondary,
-            borderRadius: BorderRadius.circular(10)),
-        padding: const EdgeInsets.all(12),
-        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-        child: ListTile(
-          title: Text(text),
-          leading: Checkbox(
-            activeColor: Colors.green,
-            value: isCompleted,
-            onChanged: onChanged,
-          ),
-        ));
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+          decoration: BoxDecoration(
+              color: isCompleted
+                  ? Colors.green
+                  : Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(10)),
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+          child: ListTile(
+            title: Text(text),
+            leading: Checkbox(
+              activeColor: Colors.green,
+              value: isCompleted,
+              onChanged: onChanged,
+            ),
+          )),
+    );
   }
 }
