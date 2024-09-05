b55d0e9913d8209adb6ce6d5d1db95d876aa7c57
Odhi-ambo <michaelderrick605@gmail.com>
2024-09-05 08:57:47 +0300

    my habit tile

--- lib/components/my_habit_tile.dart
+++ lib/components/my_habit_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class MyHabitTile extends StatelessWidget {
   final bool isCompleted;
@@ -12,29 +13,31 @@ class MyHabitTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        if (onChanged != null) {
-          //togggle completion status
-          onChanged!(!isCompleted);
-        }
-      },
-      child: Container(
-          decoration: BoxDecoration(
-              color: isCompleted
-                  ? Colors.green
-                  : Theme.of(context).colorScheme.secondary,
-              borderRadius: BorderRadius.circular(10)),
-          padding: const EdgeInsets.all(12),
-          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-          child: ListTile(
-            title: Text(text),
-            leading: Checkbox(
-              activeColor: Colors.green,
-              value: isCompleted,
-              onChanged: onChanged,
-            ),
-          )),
+    return Slidable(
+      child: GestureDetector(
+        onTap: () {
+          if (onChanged != null) {
+            //togggle completion status
+            onChanged!(!isCompleted);
+          }
+        },
+        child: Container(
+            decoration: BoxDecoration(
+                color: isCompleted
+                    ? Colors.green
+                    : Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(10)),
+            padding: const EdgeInsets.all(12),
+            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+            child: ListTile(
+              title: Text(text),
+              leading: Checkbox(
+                activeColor: Colors.green,
+                value: isCompleted,
+                onChanged: onChanged,
+              ),
+            )),
+      ),
     );
   }
 }
