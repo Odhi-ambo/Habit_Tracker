316e5c40fcedd0c67cc558e4b98270d746176b93
Odhi-ambo <michaelderrick605@gmail.com>
2024-09-18 08:33:26 +0300

    heatmap calender

--- lib/components/my_heat_map.dart
+++ lib/components/my_heat_map.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 
 class MyHeatMap extends StatelessWidget {
+  final Map<DateTime, int> datasets;
   final DateTime startDate;
-  const MyHeatMap({super.key, required this.startDate});
+  const MyHeatMap({super.key, required this.startDate, required this.datasets});
 
   @override
   Widget build(BuildContext context) {
     return HeatMap(
       startDate: startDate,
+      endDate: DateTime.now(),
+      datasets: datasets,
       colorsets: {
         1: Colors.green.shade200,
         2: Colors.green.shade300,
