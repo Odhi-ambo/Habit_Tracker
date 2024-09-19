4b31884ccd9c45851c88b081422981834eca64cc
Odhi-ambo <michaelderrick605@gmail.com>
2024-09-19 10:03:50 +0300

    utilities folders

--- lib/util/habit_util.dart
+++ lib/util/habit_util.dart
@@ -18,7 +18,16 @@ Map<DateTime, int> prepHeatMapDataSet(List<Habit> habits) {
   for (var habit in habits) {
     for (var date in habit.completedDays) {
       //normalize date to avoid time mismatch
-      final normalizeDate = DateTime(date.year, date.month, date.day);
+      final normalizedDate = DateTime(date.year, date.month, date.day);
+
+      //if the date already exists in the dataset, increment its count
+      if (dataset.containsKey(normalizedDate)) {
+        dataset[normalizedDate] = dataset[normalizedDate]! + 1;
+      } else {
+        //else initialize it with a count of 1
+        dataset[normalizedDate] = 1;
+      }
     }
   }
+  return dataset;
 }
